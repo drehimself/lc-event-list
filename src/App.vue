ec952375384207cb60d57dfb306213f04624ab6a
Andre Madarang <andre_madarang@hotmail.com>
2021-08-22 23:24:52 -0400

    Table layout complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,214 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container mx-auto px-4 xl:px-48 my-8">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam eius iure perspiciatis maxime voluptas itaque nulla praesentium? Totam architecto minima laborum dolorum quos ducimus et. Saepe dolores, aperiam in, dicta molestias non sequi dolorem pariatur perferendis dolor porro quibusdam tempore eligendi, quo illum necessitatibus iste assumenda voluptas voluptates labore quos?
+
+    <div class="flex my-12">
+      <div class="w-40 bg-gray-900 text-white text-sm divide-y divide-white border-r border-white">
+        <div class="h-10">&nbsp;</div>
+        <a href="#" class="h-16 flex justify-between hover:text-gray-200 px-4 py-2">
+          <span>Besthesda Global</span>
+          <div class="pt-1">
+            <svg class="h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path data-v-07bee6f7="" fill="currentColor" d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" class=""></path></svg>
+          </div>
+        </a>
+        <a href="#" class="h-16 flex justify-between hover:text-gray-200 px-4 py-2">
+          <span>Besthesda Global</span>
+          <div class="pt-1">
+            <svg class="h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path data-v-07bee6f7="" fill="currentColor" d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" class=""></path></svg>
+          </div>
+        </a>
+        <a href="#" class="h-16 flex justify-between hover:text-gray-200 px-4 py-2">
+          <span>Besthesda France</span>
+          <div class="pt-1">
+            <svg class="h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path data-v-07bee6f7="" fill="currentColor" d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" class=""></path></svg>
+          </div>
+        </a>
+        <a href="#" class="h-16 flex justify-between hover:text-gray-200 px-4 py-2">
+          <span>Quakecon Community</span>
+          <div class="pt-1">
+            <svg class="h-4 w-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="twitch" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path data-v-07bee6f7="" fill="currentColor" d="M391.17,103.47H352.54v109.7h38.63ZM285,103H246.37V212.75H285ZM120.83,0,24.31,91.42V420.58H140.14V512l96.53-91.42h77.25L487.69,256V0ZM449.07,237.75l-77.22,73.12H294.61l-67.6,64v-64H140.14V36.58H449.07Z" class=""></path></svg>
+          </div>
+        </a>
+      </div>
+      <div class="w-full bg-gray-500 divide-y divide-white overflow-x-auto overflow-y-hidden">
+        <div class="h-10 bg-gray-900 text-white text-sm flex divide-x divide-white w-table">
+          <div class="w-32 flex justify-center items-center">8:00 AM</div>
+          <div class="w-32 flex justify-center items-center">8:30 AM</div>
+          <div class="w-32 flex justify-center items-center">9:00 AM</div>
+          <div class="w-32 flex justify-center items-center">9:30 AM</div>
+          <div class="w-32 flex justify-center items-center">10:00 AM</div>
+          <div class="w-32 flex justify-center items-center">10:30 AM</div>
+          <div class="w-32 flex justify-center items-center">11:00 AM</div>
+          <div class="w-32 flex justify-center items-center">11:30 AM</div>
+          <div class="w-32 flex justify-center items-center">12:00 PM</div>
+          <div class="w-32 flex justify-center items-center">12:30 PM</div>
+          <div class="w-32 flex justify-center items-center">1:00 PM</div>
+          <div class="w-32 flex justify-center items-center">1:30 PM</div>
+          <div class="w-32 flex justify-center items-center">2:00 PM</div>
+          <div class="w-32 flex justify-center items-center">2:30 PM</div>
+          <div class="w-32 flex justify-center items-center">3:00 PM</div>
+          <div class="w-32 flex justify-center items-center">3:30 PM</div>
+          <div class="w-32 flex justify-center items-center">4:00 PM</div>
+          <div class="w-32 flex justify-center items-center">4:30 PM</div>
+          <div class="w-32 flex justify-center items-center">5:00 PM</div>
+          <div class="w-32 flex justify-center items-center">5:30 PM</div>
+        </div>
+        <div class="h-16 flex divide-x divide-white w-table">
+          <button class="event-block text-left w-64 h-15 bg-red-600 hover:bg-red-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+              <div class="pl-4">
+                <div class="font-semibold">Fallout 76 - UK Stream Team Build-a-thon</div>
+                <div class="text-xs text-gray-200">8:00 AM EDT - 9:00 AM EDT</div>
+              </div>
+            </button>
+
+            <div class="event-block w-32 h-16"></div>
+            <div class="event-block w-32 h-16"></div>
+
+            <button class="event-block text-left w-64 h-15 bg-red-600 hover:bg-red-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+              <div class="pl-4">
+                <div class="font-semibold">Let's talk Quake</div>
+                <div class="text-xs text-gray-200">10:00 AM EDT - 11:00 AM EDT</div>
+              </div>
+            </button>
+
+            <div class="event-block w-32 h-16"></div>
+            <div class="event-block w-32 h-16"></div>
+            <div class="event-block w-32 h-16"></div>
+            <div class="event-block w-32 h-16"></div>
+
+            <button class="event-block text-left w-64 h-15 bg-red-600 hover:bg-red-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+              <div class="pl-4">
+                <div class="font-semibold">DEATHLOOP Meets its Makers</div>
+                <div class="text-xs text-gray-200">1:00 PM EDT - 2:00 PM EDT</div>
+              </div>
+            </button>
+
+            <div class="event-block w-32 h-16"></div>
+            <div class="event-block w-32 h-16"></div>
+
+            <button class="event-block text-left w-64 h-15 bg-red-600 hover:bg-red-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+              <div class="pl-4">
+                <div class="font-semibold">Exclusive Musical Performance by Trivium</div>
+                <div class="text-xs text-gray-200">3:00 PM EDT - 4:00 PM EDT</div>
+              </div>
+            </button>
+        </div>
+        <div class="h-16 flex divide-x divide-white w-table">
+          <div class="event-block w-32 h-16"></div>
+          <div class="event-block w-32 h-16"></div>
+          <div class="event-block w-32 h-16"></div>
+
+          <button class="event-block text-left w-32 h-15 bg-yellow-600 hover:bg-yellow-800 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Countdown / QuakeCon Warm Up</div>
+              <div class="text-xs text-gray-200">9:30 AM EDT - 10:00 AM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-128 h-15 bg-yellow-600 hover:bg-yellow-800 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Bethesda Benelux Next Streamer:</div>
+              <div class="text-xs text-gray-200">10:00 AM EDT - 12:00 PM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-32 h-15 bg-yellow-600 hover:bg-yellow-800 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Interview with Arkane Lyon</div>
+              <div class="text-xs text-gray-200">12:00 PM EDT - 12:30 PM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-64 h-15 bg-yellow-600 hover:bg-yellow-800 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Cosplay Workshop & Catwalk</div>
+              <div class="text-xs text-gray-200">12:30 PM EDT - 1:30 PM EDT</div>
+            </div>
+          </button>
+
+        </div>
+        <div class="h-16 flex divide-x divide-white w-table">
+          <div class="event-block w-32 h-16"></div>
+          <div class="event-block w-32 h-16"></div>
+
+          <button class="event-block text-left w-64 h-15 bg-blue-500 hover:bg-blue-600 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">QuakeCon Day 1 - Recap & Discussion with the Bethesda France Community Team</div>
+              <div class="text-xs text-gray-200">9:00 AM EDT - 10:00 AM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-64 h-15 bg-blue-500 hover:bg-blue-600 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Cosplay Catch up & Conversation</div>
+              <div class="text-xs text-gray-200">10:00 AM EDT - 11:00 PM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-96 h-15 bg-blue-500 hover:bg-blue-600 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Fallout Lore Quiz</div>
+              <div class="text-xs text-gray-200">11:00 AM EDT - 12:30 PM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-96 h-15 bg-blue-500 hover:bg-blue-600 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">All Things DEATHLOOP</div>
+              <div class="text-xs text-gray-200">12:30 PM EDT - 2:00 PM EDT</div>
+            </div>
+          </button>
+        </div>
+
+        <div class="h-16 flex divide-x divide-white w-table">
+          <button class="event-block text-left w-32 h-15 bg-indigo-600 hover:bg-indigo-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Good Morning QuakeCon</div>
+              <div class="text-xs text-gray-200">8:00 AM EDT - 8:30 AM EDT</div>
+            </div>
+          </button>
+
+          <div class="event-block w-32 h-16"></div>
+
+          <button class="event-block text-left w-64 h-15 bg-indigo-600 hover:bg-indigo-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">The Elder Scrolls Online - Lorecast Trivia</div>
+              <div class="text-xs text-gray-200">9:00 AM EDT - 10:00 AM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-128 h-15 bg-indigo-600 hover:bg-indigo-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">DOOM 2016 - Slayer Arcade Challenge</div>
+              <div class="text-xs text-gray-200">10:00 AM EDT - 12:00 PM EDT</div>
+            </div>
+          </button>
+
+          <div class="event-block w-32 h-16"></div>
+
+          <button class="event-block text-left w-96 h-15 bg-indigo-600 hover:bg-indigo-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Fallout 76 - Junkashine Run Allstars Edition</div>
+              <div class="text-xs text-gray-200">12:30 PM EDT - 2:00 PM EDT</div>
+            </div>
+          </button>
+
+          <button class="event-block text-left w-128 h-15 bg-indigo-600 hover:bg-indigo-700 text-white flex items-center overflow-hidden whitespace-nowrap">
+            <div class="pl-4">
+              <div class="font-semibold">Quake Champions - Quick Draw 2v2 Finals</div>
+              <div class="text-xs text-gray-200">2:00 PM EDT - 4:00 PM EDT</div>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'table': '2560px',
+        '128': '32rem',
+      }
+    },
   },
   variants: {
     extend: {},
